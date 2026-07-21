ddd330185e2356e0b675d10ca15f6161a053b3e2
Mubshier Hussain <mubshierhussain985@gmail.com>
2026-07-22 00:34:04 +0530

    phase 1 complate

--- script.js
+++ script.js
@@ -66,3 +66,19 @@ function typeEffect() {
 }
 
 typeEffect();
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
